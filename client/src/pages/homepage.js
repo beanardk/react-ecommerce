5eda1f8e60c84cc1c3bbf381474eeb8f4582b47f
bean <bkung188@gmail.com>
2022-01-30 19:48:11 -0800

    added queries js to utils

---
+++ client/src/pages/homepage.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { Container } from '@chakra-ui/react'
+
+import Auth from '../utils/auth';
+
+const Homepage = () => {
+    return (<Container>
+
+    </Container>)
+}
