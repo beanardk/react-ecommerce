b81fdf5034243521b2f3e24249a2ed71176a127a
kdot <alvarezderrick0@gmail.com>
2022-01-30 18:14:29 -0800

    hashing user password

--- server/models/Account.js
+++ server/models/Account.js
@@ -33,6 +33,19 @@ const accountSchema = new Schema({
     ]
 });
 
+accountSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  accountSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+
 const Account = model('Account', accountSchema);
 
 module.exports = Account;
