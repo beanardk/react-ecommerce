cfe17cfdad9e2f73cec0f4da620b8c29980b18c1
bean <bkung188@gmail.com>
2022-01-31 12:43:08 -0800

    added navbar framework and product details

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Signup from './pages/signup';
 import Login from './pages/login';
-
+import navbar from './components/navbar';
 import { Box } from '@chakra-ui/react'
 import { Container } from '@chakra-ui/react'
 
--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -3,10 +3,8 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { Container } from '@chakra-ui/react'
 
-import Auth from '../utils/auth';
-
 const Homepage = () => {
     return (<Container>
-
+        
     </Container>)
 }
