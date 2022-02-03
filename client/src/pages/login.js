123d348ac55170c9341b29cc8dc4134dddf1a1f2
beanardk <90477691+beanardk@users.noreply.github.com>
2022-02-03 10:15:19 -0800

    Revert "finished homepage"

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_ACCOUNT } from '../utils/mutations';
+import Navbar from '../components/navbar';
 
 import Auth from '../utils/auth';
 
