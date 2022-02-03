47fba575bf0114639b8394351b854907ed51572b
kdot <alvarezderrick0@gmail.com>
2022-02-03 13:40:03 -0800

    removed uneccesary imports

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import Signup from './pages/signup';
 import Login from './pages/login';
 import Home from './pages/homepage'
 import Cart from './pages/cart'
+import Product from './pages/products';
+import Header from './components/header';
 
 
 const httpLink = createHttpLink({
@@ -40,6 +42,8 @@ const httpLink = createHttpLink({
     return (
       <ApolloProvider client={client}>
         <Router>
+          <div>
+            <Header />
             <div>
               <Route exact path="/">
                 <Home/>
@@ -53,10 +57,23 @@ const httpLink = createHttpLink({
                 <Signup />
               </Route>
 
-              <Route exact path="/cart">
-                <Cart />
+            
+              <Route exact path="/products">
+                <Product
+                  _id="120A"
+                  description="Such a warm tshirt!"
+                  price={5.99}
+                  image="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" 
+                  />
+                <Product
+                  _id="120A"
+                  description="Such a warm tshirt!"
+                  price={5.99}
+                  image="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" 
+                  />
               </Route>
             </div>
+          </div>
         </Router>
       </ApolloProvider>
     );
