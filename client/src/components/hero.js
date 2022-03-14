1908f3955ea4d7de9240d262cbcd9712ebfd38ea
Sebastian Lample <SebastianLample@gmail.com>
2022-03-14 12:37:31 -0700

    centered explore button, added blur to increase visibility

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -7,39 +7,51 @@ import {
     // useBreakpointValue,
   } from '@chakra-ui/react';
   
-  export default function hero() {
+  export default function Hero() {
     return (
       <Flex
-        w={'full'}
         h={'90vh'}
         backgroundImage={
           'url(https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/streetwear-brands-1627934788.jpg)'
         }
         backgroundSize={'cover'}
-        backgroundPosition={'center center'}>
-        <VStack
-          w={'full'}
-          justify={'center'}
-          // px={useBreakpointValue({ base: 4, md: 8 })}
-          bgGradient={'linear(to-r, blackAlpha.600, transparent)'}>
-          <Stack maxW={'2xl'} align={'flex-start'} spacing={5}>
-            <Text
-              color={'white'}
-              fontWeight={700}
-              lineHeight={1.2}>
-              Our new Spring/Summer Collection
-            </Text>
-            <Stack direction={'row'}>
-                <Button
-                  bg={'blue.400'}
-                  rounded={'full'}
-                  color={'white'}
-                  _hover={{ bg: 'blue.500' }}>
-                  Explore
-                </Button>
-            </Stack>
-          </Stack>
-        </VStack>
+        backgroundPosition={'center center'}
+        >
+        
+          <VStack
+            w={'full'}
+            justify={'center'}
+            textAlign={'center'}
+            // px={useBreakpointValue({ base: 4, md: 8 })}
+            bgGradient={'linear(to-r, blackAlpha.400, transparent)'}
+            >
+              <Stack 
+                maxW={'2xl'} 
+                spacing={5}
+                backdropFilter={'blur( 1.5px );'}
+                borderRadius={'10px'}
+                p={'3%'}
+              >
+                
+                <Text
+                  color={'aliceblue'}
+                  fontWeight={700}
+                  fontSize={'20px'}
+                  lineHeight={1.2}>
+                  Our new Spring/Summer Collection
+                </Text>
+
+                  <Button
+                    bg={'blue.400'}
+                    rounded={'full'}
+                    color={'white'}
+                    _hover={{ bg: 'blue.500' }}>
+                    Explore
+                  </Button>
+              
+              </Stack>
+
+            </VStack>
       </Flex>
     );
   }
