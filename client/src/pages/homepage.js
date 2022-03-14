5bec35c95e82c5962a96871e53f2c7f1baf20249
Sebastian Lample <SebastianLample@gmail.com>
2022-03-14 12:38:02 -0700

    Removed useless tags

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import {
-Box,
-Spacer,
-} from '@chakra-ui/react'
 import Hero from '../components/hero';
 
 const Homepage = () => {
 
     return (
-        <main>
-            <div>
-                <Spacer />
-
-                <Box>
-                    <Hero/>
-                </Box>
-            </div>
-        </main>
+        <Hero/>
     )
 };
 
