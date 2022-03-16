b597b4d14db0e0c7396a982e0a0d496972909306
Sebastian Lample <SebastianLample@gmail.com>
2022-03-16 01:04:14 -0700

    Shorted explore button length by half, added a very slight blue hue to the background to increase text visibility

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -4,6 +4,7 @@ import {
     Button,
     Text,
     VStack,
+    Center
     // useBreakpointValue,
   } from '@chakra-ui/react';
   
@@ -25,29 +26,33 @@ import {
             // px={useBreakpointValue({ base: 4, md: 8 })}
             bgGradient={'linear(to-r, blackAlpha.400, transparent)'}
             >
-              <Stack 
+              <Stack
                 maxW={'2xl'} 
                 spacing={5}
                 backdropFilter={'blur( 1.5px );'}
+                background={'rgba( 140, 190, 244, 0.08 )'}
                 borderRadius={'10px'}
                 p={'3%'}
               >
                 
                 <Text
-                  color={'aliceblue'}
+                  color={'white'}
                   fontWeight={700}
                   fontSize={'20px'}
                   lineHeight={1.2}>
                   Our new Spring/Summer Collection
                 </Text>
 
-                  <Button
+                  <Center>
+                    <Button
+                    maxW={'50%'}
                     bg={'blue.400'}
                     rounded={'full'}
                     color={'white'}
                     _hover={{ bg: 'blue.500' }}>
                     Explore
                   </Button>
+                  </Center>
               
               </Stack>
 
